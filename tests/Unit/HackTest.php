67c82c086d3e0e00ba340cbfff04ed02608ec95e
Njazi Shehu <shehu.nj@gmail.com>
2022-04-25 23:36:02 +0200

    added unit test for single hack path

---
+++ tests/Unit/HackTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Hack;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class HackTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_has_a_path()
+    {
+        $hack = Hack::factory()->create();
+        $this->assertSame($hack->path(), 'hacks/' . $hack->id);
+    }
+
+}
