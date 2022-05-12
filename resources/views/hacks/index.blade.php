7feef2d2adc267c97bcdf04bbb42378f4d3655a2
Njazi Shehu <shehu.nj@gmail.com>
2022-05-13 01:29:16 +0200

    modified style and mobile layout

--- resources/views/hacks/index.blade.php
+++ resources/views/hacks/index.blade.php
@@ -3,13 +3,12 @@
         <ol class="pl-1">
             @foreach($hacks as $hack)
                 <li>
-
-                    <div class="flex items-center space-x-1">
-                        <span class="text-center text-xs border-red-500 border h-4 w-4 hover:bg-red-500 hover:text-white cursor-pointer">
+                    <div class="grid grid-cols-12 md:flex items-center space-x-1">
+                        <span class="col-span-1 text-center text-xs border-red-500 border h-4 w-4 hover:bg-red-500 hover:text-white cursor-pointer">
                             H
                         </span>
-                        <a class="text-gray-900" href="{{route('hack.show', $hack)}}"> {{ $hack->title }} </a>
-                        <a class="" href="{{$hack->url}}" target="_blank">
+                        <a class="col-span-10 text-gray-900" href="{{route('hack.show', $hack)}}"> {{ $hack->title }} </a>
+                        <a class="col-span-1" href="{{$hack->url}}" target="_blank">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                             </svg>
